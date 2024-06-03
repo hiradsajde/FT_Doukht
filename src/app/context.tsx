d57f9af5a50496c1598d6853e4fa401446bdf57a
hiradsajde <hi@hiradsajde.ir>
2024-06-03 12:18:16 +0330

    bug fixed

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -9,36 +9,26 @@ export const cartContext: any = createContext([]);
 export function Provider({ children }: { children: React.ReactNode }) {
   const reducer = (state: any, action: any) => {
     switch (action.do) {
-      case "init":
-        return {...state , slugs : action.payload}
       case "open":
         return { ...state, open: true };
-        
       case "close":
         return { ...state, open: false };
-        
       case "add":
-        if (state.slugs?.includes(action.slug)) {
+        if (state.slugs.includes(action.slug)) {
           return { ...state };
         }
         return { ...state, slugs: [...state.slugs, action.slug] };
-        
       case "remove":
         return {...state , slugs : state.slugs.splice(state.slugs.indexOf(action.slug), 1)};
-        
     }
     return state;
   };
   const [state, dispatch] = useReducer(reducer, { slugs: [], open: false });
   useEffect(() => {
-    if(JSON.parse(sessionStorage.getItem('cart') || "{}")){
-      dispatch({do:'init' , payload:JSON.parse(sessionStorage.getItem('cart') || "{}")})
-    } else {
-      dispatch({do:'init' , payload:[]})
-    }
+    state.slugs = JSON.parse(sessionStorage.getItem('cart') ?? "[]")
   } , [])
   useEffect(() => {
-    sessionStorage.setItem('cart' , JSON.stringify(state.slugs))
+    sessionStorage.setItem('cart' , Array.isArray(state.slugs) ? JSON.stringify([...state.slugs]) : "[]")
   } , [state])
   return (
     <ApolloProvider client={client}>

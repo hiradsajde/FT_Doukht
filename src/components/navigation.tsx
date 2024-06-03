f4efec242473cfba8ca71e60b4d3b5409ccb861e
hiradsajde <hi@hiradsajde.ir>
2024-06-03 07:04:41 +0330

    hover effects added to buttons

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,10 +9,10 @@ import { useContext } from "react";
 export default function Navigation(){
     const [cartData , setCartData] : any = useContext(cartContext)
     return <nav className="w-full mb-10 shadow-md h-14 flex items-center justify-between px-6">
-        <div className="mx-4">
+        <div className="mx-4 hover:cursor-pointer">
             <Image src={"/logo.png"} alt="logo" className="h-8" width={140} height={70}/>
         </div>
-        <div onClick={() => setCartData({do : 'open'})}>
+        <div className="hover:cursor-pointer" onClick={() => setCartData({do : 'open'})}>
             <FontAwesomeIcon icon={faShoppingBag} className="w-8 h-8 text-primary"/>
         </div>
     </nav>
